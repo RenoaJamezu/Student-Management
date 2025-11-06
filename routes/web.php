32564ba28db0b19e67b0258cb716dc6b20f83794
Kurasao <jamerolenor@gmail.com>
2025-11-06 22:03:19 +0800

    -introduction to blade templates
    2 ways to create blade file:
    -add file directly in view folder
    -in terminal run: php artisan make:view [name of the file]

--- routes/web.php
+++ routes/web.php
@@ -13,36 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-// introduction
 Route::get('/', function () {
-  return 'Hello from laravel';
+  return view('welcome');
 });
 
-Route::get('about', function () {
-  return 'About us';
+Route::get('about-us', function () {
+  return view('aboutus');
 });
 
-// grouping / naming
-Route::prefix('details')->group(function () {
-  Route::get('teacher', function () {
-    return 'Teacher details';
-  })->name('teacher-Details');
-  Route::get('student', function () {
-    return 'Student details';
-  })->name('student-Details');
-});
-
-// parameter
-Route::get('student/{id}/{reg}', function ($id, $reg) {
-  return 'student number ' . $id . ' registration number ' . $reg;
-});
-
-// fallback
-Route::fallback(function() {
-  return 'this page is not found please try again.';
-});
+Route::view('contact-us', 'contactus');
